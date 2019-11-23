8f0d7edf504621ed7ba9c6e44f359914f1c9b4ed
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2019-11-22 18:25:48 -0800

    Fixed makeStudentArray function

--- js/script.js
+++ js/script.js
@@ -32,67 +32,69 @@ let pageArray = [];
 //Still needs conditionals
 
 function makeStudentArray(list) {
-  for (let i = 0; i < li.length; i++) {
-  studentArray += studentArray.push(li[i]);
-} return studentArray;
+  for (let i = 0; i < list.length; i++) {
+    studentArray.push(list[i]);
+  }  return studentArray;
 }
+console.log(makeStudentArray(li));
 /*
 //Main function
 function showPage() {
-//This function creates a loop that assigns all the li elements into an array
 
-        
 //This function contains the logic behind setting ten li elements per page. Math.ceil assures that there will be six pages for 54 items.
   function getNumberOfPages(array) {
-    return Math.ceil(studentArray.length / studentsPerPage);
+    return Math.ceil(array.length / studentsPerPage);
     }
     
  /* function pageOne() {
-    currentPage += 1;
+    currentPage = 1;
     loadList();
 }
     
   function pageTwo() {
-    currentPage += 2;
+    currentPage = 2;
     loadList();
 }
 
   function pageThree() {
-    currentPage += 3;
+    currentPage = 3;
     loadList();
 }
 
   function pageFour() {
-    currentPage += 4;
+    currentPage = 4;
     loadList();
 }
 
   function pageFive() {
-    currentPage += 5;
+    currentPage = 5;
     loadList();
 }
 
   function pageSix() {
-    currentPage += 6;
+    currentPage = 6;
     loadList(); 
 }*/
 //This function sets how many li elements appear per page. Example- page 1: begin = (1-1) * 10 = 0. End = 0 + 10. pageArray lists li[0-10].
- /*
+//--WORKING
   function loadList() {
     let begin = ((currentPage - 1) * studentsPerPage);
     let end = begin + studentsPerPage;
     pageArray = studentArray.slice(begin, end);
     return pageArray;
 }
+console.log(loadList());
 
+//--WORKING
 //This is the logic to display the li items onto the page
   function createList() {
     ul.innerHTML = "";
     for (let j = 0; j < pageArray.length; j++) {
     ul.innerHTML += pageArray[j] + "<br>";
+}return ul;
 }
-}
-        
+console.log(createList());
+ /*       
 }
         
 /*** 
@@ -108,33 +110,33 @@ function appendPageLinks() {
   ul.appendChild(pg1);
   
   const pg2 = document.create("button");
-  pg1.id = "second";
-  pg1.value = pageTwo();
-  pg1.innerHTML = "2";
+  pg2.id = "second";
+  pg2.value = pageTwo();
+  pg2.innerHTML = "2";
   ul.appendChild(pg2);
   
   const pg3 = document.create("button");
-  pg1.id = "third";
-  pg1.value = pageThree();
-  pg1.innerHTML = "3";
+  pg3.id = "third";
+  pg3.value = pageThree();
+  pg3.innerHTML = "3";
   ul.appendChild(pg3);
   
   const pg4 = document.create("button");
-  pg1.id = "fourth";
-  pg1.value = pageFour();
-  pg1.innerHTML = "4";
+  pg4.id = "fourth";
+  pg4.value = pageFour();
+  pg4.innerHTML = "4";
   ul.appendChild(pg4);
   
   const pg5 = document.create("button");
-  pg1.id = "fifth";
-  pg1.value = pageFive();
-  pg1.innerHTML = "5";
+  pg5.id = "fifth";
+  pg5.value = pageFive();
+  pg5.innerHTML = "5";
   ul.appendChild(pg5);
   
   const pg6 = document.create("button");
-  pg1.id = "sixth";
-  pg1.value = pageSix();
-  pg1.innerHTML = "6";
+  pg6.id = "sixth";
+  pg6.value = pageSix();
+  pg6.innerHTML = "6";
   ul.appendChild(pg6);
 }
 
