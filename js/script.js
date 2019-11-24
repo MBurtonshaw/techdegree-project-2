b6f6ce41d9e0681ed0659f72b98d1da753a1e2d9
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2019-11-24 08:11:25 -0800

    Updated page functions

--- js/script.js
+++ js/script.js
@@ -10,8 +10,6 @@ const studentsPerPage = 10;
 let currentPage = 1;
 let numberOfPages = 1;
 let studentArray = [];
-let pageArray = [];
-
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -36,112 +34,142 @@ function makeStudentArray(list) {
     studentArray.push(list[i]);
   }  return studentArray;
 }
-console.log(makeStudentArray(li));
-/*
+
+const mainArray = makeStudentArray(li);
+
+//console.log(mainArray);
+
 //Main function
-function showPage() {
+//function showPage() {
 
 //This function contains the logic behind setting ten li elements per page. Math.ceil assures that there will be six pages for 54 items.
-  function getNumberOfPages(array) {
-    return Math.ceil(array.length / studentsPerPage);
-    }
-    
- /* function pageOne() {
-    currentPage = 1;
-    loadList();
-}
-    
-  function pageTwo() {
-    currentPage = 2;
-    loadList();
+
+function getNumberOfPages(array) {
+  return Math.ceil(array.length / studentsPerPage);
 }
 
-  function pageThree() {
-    currentPage = 3;
-    loadList();
-}
+const numPages = getNumberOfPages(mainArray);
 
-  function pageFour() {
-    currentPage = 4;
-    loadList();
-}
+console.log(numPages);
 
-  function pageFive() {
-    currentPage = 5;
-    loadList();
-}
-
-  function pageSix() {
-    currentPage = 6;
-    loadList(); 
-}*/
 //This function sets how many li elements appear per page. Example- page 1: begin = (1-1) * 10 = 0. End = 0 + 10. pageArray lists li[0-10].
 //--WORKING
-  function loadList() {
-    let begin = ((currentPage - 1) * studentsPerPage);
-    let end = begin + studentsPerPage;
-    pageArray = studentArray.slice(begin, end);
-    return pageArray;
+
+console.log(currentPage);
+
+function loadList(page) {
+  let begin = ((page - 1) * studentsPerPage);
+  let end = begin + studentsPerPage;
+  return mainArray.slice(begin, end);
 }
-console.log(loadList());
+
+const pageOne = loadList(1);
+const pageTwo = loadList(2);
+const pageThree = loadList(3);
+const pageFour = loadList(4);
+const pageFive = loadList(5);
+const pageSix = loadList(6);
+
 
 //--WORKING
 //This is the logic to display the li items onto the page
-  function createList() {
-    ul.innerHTML = "";
-    for (let j = 0; j < pageArray.length; j++) {
-    ul.innerHTML += pageArray[j] + "<br>";
-}return ul;
+function createList(pageNum) {
+  if (pageNum === 1) {
+    pageOne.style.display = "block";
+    pageTwo.style.display = "none";
+    pageThree.style.display = "none";
+    pageFour.style.display = "none";
+    pageFive.style.display = "none";
+    pageSix.style.display = "none";
+  } else if (pageNum === 2) {
+      pageOne.style.display = "none";
+      pageTwo.style.display = "block";
+      pageThree.style.display = "none";
+      pageFour.style.display = "none";
+      pageFive.style.display = "none";
+      pageSix.style.display = "none"; 
+  } else if (pageNum === 3) {
+      pageOne.style.display = "none";
+      pageTwo.style.display = "none";
+      pageThree.style.display = "block";
+      pageFour.style.display = "none";
+      pageFive.style.display = "none";
+      pageSix.style.display = "none";
+  } else if (pageNum === 4) {
+      pageOne.style.display = "none";
+      pageTwo.style.display = "none";
+      pageThree.style.display = "none";
+      pageFour.style.display = "block";
+      pageFive.style.display = "none";
+      pageSix.style.display = "none";
+  } else if (pageNum === 5) {
+      pageOne.style.display = "none";
+      pageTwo.style.display = "none";
+      pageThree.style.display = "none";
+      pageFour.style.display = "none";
+      pageFive.style.display = "block";
+      pageSix.style.display = "none";
+  } else if (pageNum === 6) {
+      pageOne.style.display = "none";
+      pageTwo.style.display = "none";
+      pageThree.style.display = "none";
+      pageFour.style.display = "none";
+      pageFive.style.display = "none";
+      pageSix.style.display = "block";
+  }  
 }
-console.log(createList());
- /*       
+
+console.log(createList(pageSix));
+
+ /*
 }
-        
-/*** 
+
+/***
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+
 /*
 function appendPageLinks() {
-  const pg1 = document.create("button");
+  const pg1 = document.createElement("BUTTON");
   pg1.id = "first";
   pg1.value = pageOne();
   pg1.innerHTML = "1";
   ul.appendChild(pg1);
   
-  const pg2 = document.create("button");
+  const pg2 = document.createElement("BUTTON");
   pg2.id = "second";
   pg2.value = pageTwo();
   pg2.innerHTML = "2";
   ul.appendChild(pg2);
   
-  const pg3 = document.create("button");
+  const pg3 = document.createElement("BUTTON");
   pg3.id = "third";
   pg3.value = pageThree();
   pg3.innerHTML = "3";
   ul.appendChild(pg3);
   
-  const pg4 = document.create("button");
+  const pg4 = document.createElement("BUTTON");
   pg4.id = "fourth";
   pg4.value = pageFour();
   pg4.innerHTML = "4";
   ul.appendChild(pg4);
   
-  const pg5 = document.create("button");
+  const pg5 = document.createElement("BUTTON");
   pg5.id = "fifth";
   pg5.value = pageFive();
   pg5.innerHTML = "5";
   ul.appendChild(pg5);
   
-  const pg6 = document.create("button");
+  const pg6 = document.createElement("BUTTON");
   pg6.id = "sixth";
   pg6.value = pageSix();
   pg6.innerHTML = "6";
   ul.appendChild(pg6);
 }
 
+appendPageLinks();
 
 */
-
 //console.log(showPage());
 //^Throws undefined error
