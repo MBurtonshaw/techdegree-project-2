48e0c21671e83cc46b1d1ea2645e51a87bf87e98
MBurtonshaw <56322461+MBurtonshaw@users.noreply.github.com>
2019-11-26 17:14:40 -0800

    Created non-functioning loop in appendPageLinks()

--- js/script.js
+++ js/script.js
@@ -6,26 +6,11 @@ FSJS project 2 - List Filter and Pagination
 const li = document.getElementsByClassName("student-item");
 const studentsPerPage = 10;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-//Right now, the function is targeting everything in the list, not just 1-10
-//Goal is, if page = #, [i].display = block, else [i].display = hidden
-//Six if statements, ending with an else
 function showPage(list, page) {
-    let begin = (page-1) * studentsPerPage;    //Sets number of list items per page; example: page (1-1) * 10 = 0... 0 + 10 = 10... 0-10 items
+//Sets number of list items per page; example: page (1-1) * 10 = 0... 0 + 10 = 10... 0-10 items
+    
+    let begin = (page-1) * studentsPerPage;    
     let end = begin + studentsPerPage;
     for (let i = 0; i < list.length; i++) {     
         if (i >= begin && i < end) {            
@@ -36,20 +21,46 @@ function showPage(list, page) {
     }
 }
 
+
 /***
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
-function appendPageLinks() {
-    
-    function createButton(page) {
-        let ul = document.getElementsByClassName('student-list')[0]; //Must specify placement within ul to append; hence, [0]
-        let button = document.createElement("BUTTON");
-        button.textContent = page;
-        ul.appendChild(button);
-        button.addEventListener ( "click", (e) => {
-            if (e.target.tagName === "BUTTON") {
+
+
+function appendPageLinks(list) {
+
+    function createButton() {
+      let totalPages = Math.ceil(list.length / studentsPerPage);
+      const newDiv = document.createElement("DIV");
+      newDiv.className = "pagination";
+        //must be set to [0] because it is a collection of HTML elements, not one node in itself
+      let otherDiv = document.getElementsByClassName("page")[0];
+      let ul = document.createElement("UL");
+      newDiv.appendChild(ul);
+      otherDiv.appendChild(newDiv);
+      
+      for ( let i = 0; i < totalPages; i++) {
+ 
+      let button = document.createElement("LI");
+      let a = document.createElement("A");
+      ul.appendChild(button);
+      button.appendChild(a);
+      a.textContent = i;
+      a.href = "#";
+      createButton(i);
+         
+      } showPage(list);
+    }   
+}
+
+appendPageLinks(li);
+
+//Code to be recycled:
+        /*
+        li.addEventListener ( "click", (e) => {
+            if (e.target.className === "pagination") {
                 const pageButton = e.target;
                 if (pageButton.textContent === "1") {
                     showPage(li, 1);
@@ -65,16 +76,14 @@ function appendPageLinks() {
                     showPage(li, 6);
                 }
             }
-        });
-    }
-    
+            });
+
     createButton("1");
     createButton("2");
     createButton("3");
     createButton("4");
     createButton("5");
     createButton("6");
-}
 
-appendPageLinks();
-
+*/
+//appendPageLinks();
